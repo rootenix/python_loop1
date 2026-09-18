e2ecad0b5a0de677ded931ea46a2a334f026f9b6
rootenix <azamatyashinovv@gmail.com>
2026-09-18 07:02:24 -0700

    LOOPS

---
+++ main.py
@@ -0,0 +1,19 @@
+# for i in range(1,101):
+#     if i%3==0 and i%5==0:
+#         print("fizzbazz")
+#     elif i%3==0:
+#         print("fizz")
+#     elif i%5==0:
+#         print("bazz")
+#     else:
+#         print(i)
+
+
+# yil = int(input("yilni kiriting"))
+
+# if yil%400==0:
+#     print("kabisa"    )
+# elif yil%4==0 and yil%100!=0:
+#     print("kabisa yil")
+# else:
+#     print("oddiy yil")
